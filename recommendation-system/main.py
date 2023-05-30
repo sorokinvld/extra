cd2dc9ad73adcbc98aee2ced2cfad28275049347
akatheos <akatheos11@gmail.com>
2023-05-30 16:25:09 +0100

    recommendation system working? i guess?

--- recommendation-system/main.py
+++ recommendation-system/main.py
@@ -1,6 +1,11 @@
+import json
+from bson import ObjectId
 import requests
 from pymongo.mongo_client import MongoClient
 from flask import Flask, request
+import bson.json_util as json_util
+import pandas as pd
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 # Sentiment function API
@@ -40,40 +45,54 @@ for review in user_reviews:
     userid = review["user_id"]
     for rating in ratingcollection.find({"item_id": hotelid, "user_id": userid}):
         rate = rating["rating"]
-    # Implement sentiment analysis function
-    sentiment = query({
+     # Implement sentiment analysis function
+    sentimentQuery = query({
         "inputs": preprocessed_comment,
     })
+    sentiment = sentimentQuery[0][0]["label"]
+    if (sentiment == "POSITIVE"):
+        sentiment_value = 1
+    else:
+        sentiment_value = -1
     # Create a new dictionary with the processed data
     processed_review = {
-        'hotelid':  hotelid,
         'userid': userid,
-        'review': preprocessed_comment,
+        'hotelid':  hotelid,
         'rating': rate,
-        'sentiment': sentiment,
+        'sentiment_value': sentiment_value
     }
     preprocessed_reviews.append(processed_review)
 
+print(preprocessed_reviews)
 
-user_profiles = {}
 
-for review in preprocessed_reviews:
-    user_id = review['userid']
+def recommend_hotels(user_id):
 
-    # Check if user profile exists, create one if not
-    if user_id not in user_profiles:
-        user_profiles[user_id] = {
-            'hotels': [],
-            'reviews': [],
-            'ratings': [],
-            'sentiments': []
-        }
+    # Convert data to a DataFrame
+    df = pd.DataFrame(preprocessed_reviews)
+    # Remove duplicate entries
+    df = df.drop_duplicates(subset=['hotelid', 'userid'])
+    print(df)
+    # Pivot the data to create a user-item matrix
+    user_item_matrix = df.pivot(
+        index='userid', columns='hotelid', values=['rating']).fillna(0)
+    # Calculate the cosine similarity between users
+    user_similarity = cosine_similarity(user_item_matrix)
+    # Get the index of the target user
+    target_user_index = df[df['userid'] == ObjectId(user_id)].index[0]
+    # Get the similarity scores of the target user compared to other users
+    target_user_similarity_scores = user_similarity[target_user_index]
+    # Sort the similarity scores and get the indices of the most similar users
+    similar_user_indices = target_user_similarity_scores.argsort()[::-1]
+    # Get the top 5 similar users
+    top_similar_users = similar_user_indices[1:4]
+    # Get the hotels that the top similar users have rated highly
+    recommended_hotels = user_item_matrix.iloc[top_similar_users].mean(
+    ).sort_values(ascending=False)
+    # Convert the Pandas Series to a list
+    recommended_hotels_list = recommended_hotels.reset_index().values.tolist()
 
-    # Add review details to user profile
-    user_profiles[user_id]['hotels'].append(review['hotelid'])
-    user_profiles[user_id]['reviews'].append(review['review'])
-    user_profiles[user_id]['ratings'].append(review['rating'])
-    user_profiles[user_id]['sentiments'].append(review['sentiment'])
+    return recommended_hotels_list
 
 
 app = Flask(__name__)
@@ -87,8 +106,9 @@ def greetings():
 @app.route('/api/recommendation')
 def recommendation():
     id = request.args["id"]
+    recommended_hotels = recommend_hotels(id)
     # Do the recommendation algorithm using the id
-    return 'Recommended hotels'
+    return json.loads(json_util.dumps(recommended_hotels))
 
 
 if __name__ == '__main__':
