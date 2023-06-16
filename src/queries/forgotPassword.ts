83e5cd16420e0443df9238f7d38ff0dbcf9765a8
akatheos <akatheos11@gmail.com>
2023-06-16 09:12:49 +0100

    changed backend url

--- src/queries/forgotPassword.ts
+++ src/queries/forgotPassword.ts
@@ -1,7 +1,14 @@
 import axios from "axios";
 import { getError } from "./errors";
+import { z } from "zod";
 
-export const forgotPassword = async (data: any) => {
+const forgotPasswordSchema = z.object({
+  email: z.string().email(),
+});
+
+type ForgotPasswordSchema = z.infer<typeof forgotPasswordSchema>;
+
+export const forgotPassword = async (data: ForgotPasswordSchema) => {
   try {
     const response = await axios.post(
       `${process.env.NEXT_PUBLIC_SERVER_URL}/api/users/forgotpassword`,
